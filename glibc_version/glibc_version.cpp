d618b21743db33eae2204ae7f7901730a6f7d57d
Allen Guo <alleng@graphcore.ai>
2022-07-02 10:32:23 -0400

    update glibc_version

--- glibc_version/glibc_version.cpp
+++ glibc_version/glibc_version.cpp
@@ -1,7 +1,18 @@
-#include <gnu/libc-version.h>
 #include <stdio.h>
-#include <stdlib.h>
+#ifdef __GLIBC__
+#include <gnu/libc-version.h>
+#endif
 
 // https://stackoverflow.com/questions/9705660/check-glibc-version-for-a-particular-gcc-compiler
 
-int main() { printf("\nGNU libc version: %s\n\n", gnu_get_libc_version()); }
+int main(void) {
+#ifdef __GLIBC__
+  printf("\nGNU libc compile-time version: %u.%u\n", __GLIBC__,
+         __GLIBC_MINOR__);
+  printf("GNU libc runtime version:      %s\n\n", gnu_get_libc_version());
+  return 0;
+#else
+  puts("Not the GNU C Library");
+  return 1;
+#endif
+}
